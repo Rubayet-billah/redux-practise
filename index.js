bc26e41517ce69e15aa4887f3cd6015c2b13d36d
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-11 02:00:28 +0600

    reducer created

--- index.js
+++ index.js
@@ -34,3 +34,29 @@ const getTodosSuccess = (todos) => {
         payload: todos
     }
 }
+
+// reducers
+const todosReducers = (state = initialTodosState, action) => {
+    switch (action.type) {
+        case GET_TODOS_REQUEST:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case GET_TODOS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                todos: action.payload
+            }
+        case GET_TODOS_FAILED:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
